27240b37730d3a680abad7821e0a62c3142e40fe
Programatik <programatik29@gmail.com>
2021-08-26 15:18:48 +0300

    reload tls configuration

--- src/server/record.rs
+++ src/server/record.rs
@@ -6,25 +6,25 @@
 //!
 //! ```rust,no_run
 //! use std::net::SocketAddr;
-//! 
+//!
 //! use axum::{
 //!     handler::get,
 //!     extract::Extension,
 //!     Router,
 //! };
-//! 
+//!
 //! use axum_server::record::Recording;
-//! 
+//!
 //! #[tokio::main]
 //! async fn main() {
 //!     let app = Router::new().route("/", get(handler));
-//! 
+//!
 //!     axum_server::bind("127.0.0.1:3000")
 //!         .serve_and_record(app)
 //!         .await
 //!         .unwrap();
 //! }
-//! 
+//!
 //! async fn handler(
 //!     Extension(addr): Extension<SocketAddr>,
 //!     Extension(rec): Extension<Recording>,
