8e3bc5cbf45ab6cc735f7f7daa4ed2debc7eac7e
Programatik <programatik29@gmail.com>
2021-08-29 16:04:28 +0300

    update tls_rustls example

--- examples/tls_rustls.rs
+++ examples/tls_rustls.rs
@@ -2,15 +2,11 @@
 
 use axum::{handler::get, Router};
 
-use axum_server::Server;
-
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, world" }));
 
-    Server::new()
-        .bind("127.0.0.1:3000")
-        .bind_rustls("127.0.0.1:3443")
+    axum_server::bind_rustls("127.0.0.1:3000")
         .private_key_file("examples/self-signed-certs/key.pem")
         .certificate_file("examples/self-signed-certs/cert.pem")
         .serve(app)
